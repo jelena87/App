46dd855b75aa3a5a12d5d82977504755d349aa9d
Vladan Milinkovic <Vlada@Dovla.local>
2017-07-31 11:35:15 +0200

    fields

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     var drag_depo = ['drag1','drag2'];
-    var drag_cont = ['text_box'];
+    var drag_cont = ['text_box','text_area','html_editor','check_box','datapicker','dropdown','radiobutton','lookup','grid','label'];
     var isLeft;
     if(drag_depo.includes(data)){
         isLeft = 'depositories';
@@ -67,15 +67,18 @@ function drop(ev) {
              element.innerHTML ="<div class='block'></div>";
              return element;
          }
-         if(data === 'drag2' && element.parentNode.classList.value === 'block') {
+         else if(data === 'drag2' && element.parentNode.classList.value === 'block') {
              element.classList.add("cards","col-md-4");
              //element.innerHTML ="<div class='col-md-4 col-xs-12 card'>hey2!</div>";
              return element;
          }
-        if(data === 'drag2' && element.parentNode.id ==='frame') {
+        else if(data === 'drag2' && element.parentNode.id ==='frame') {
             element.classList.add("block");
             element.innerHTML ="<div class='cards col-md-4'></div>";
             return element;
+        }
+        else {
+            element.remove();
         }
 
     }
@@ -84,22 +87,77 @@ function drop(ev) {
         var element = document.createElement("div");
         ev.target.appendChild(element);
 
-        if(data === 'text_box' && element.parentNode.classList.value === 'cards col-md-4'){
+        //Create templates
+        var text_box = "<input type='text'>";
+        var text_area ="<textarea rows='4' ></textarea>";
+        var html_editor="";
+        var check_box = "<input type='checkbox'>";
+        var datapicker = "<input type='date'>";
+        var dropdown = "";
+        var radiobutton="<input type='radio'>";
+        var lookup="";
+        var grid="";
+        var label="<label></label>";
+
+        var group_field;
+
+
+        var template;
+
+        //Get dragged element and set template for this element
+        switch (data) {
+            case 'text_box':
+                template = text_box;
+                break;
+            case 'text_area':
+                template = text_area;
+                break;
+            case 'html_editor':
+                template = html_editor;
+                break;
+            case 'check_box':
+                template = check_box;
+                break;
+            case 'datapicker':
+                template = datapicker;
+                break;
+            case 'dropdown':
+                template = dropdown;
+                break;
+            case 'radiobutton':
+                template = radiobutton;
+                break;
+            case 'lookup':
+                template = lookup;
+                break;
+            case 'grid':
+                template = grid;
+                break;
+            case 'label':
+                template = label;
+                break;
+        }
+        console.log(template);
+        //Textbox
+        if( element.parentNode.classList.value === 'cards col-md-4'){
             element.classList.add("field");
-            element.innerHTML ="<input type='text'>";
+            element.innerHTML = template;
             return element;
 
         }
-        if(data === 'text_box' && element.parentNode.classList.value === 'block'){
+        if(element.parentNode.classList.value === 'block'){
             element.classList.add("cards","col-md-4");
-            element.innerHTML ="<div class='field'><input type='text'></div>";
+            element.innerHTML ="<div class='field'>"+ template + "</div>";
             return element;
 
         }
-        if(data === 'text_box' && element.parentNode.id ==='frame'){
+        if(element.parentNode.id ==='frame'){
             element.classList.add("container-fluid","row");
-            element.innerHTML ="<div class='block'><div class='cards col-md-4'><div class='field'><input type='text'></div></div></div>";
+            element.innerHTML ="<div class='block'><div class='cards col-md-4'><div class='field'>" + template +"</div></div></div>";
             return element;
+        }
+        if(element.parentNode.classList.value !== 'cards col-md-4' && element.parentNode.classList.value !== 'block' && element.parentNode.id != 'frame') {
+            element.remove();
         }
 
     }
@@ -110,4 +168,8 @@ function drop(ev) {
 $("#frame").sortable({
     axis: "y",
     items: ".row"
+});
+$(".container-fluid").sortable({
+    axis: "x",
+    items: ".block"
 });
