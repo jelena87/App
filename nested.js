b22aeae618d63cc9f101f3e22226c151cba57cdf
Jelena <jelena@software-nation.com>
2017-08-02 15:28:14 +0200

    moving cards in a grid

--- nested.js
+++ nested.js
@@ -30,23 +30,6 @@ window.onclick = function(event) {
         }
     }
 };
-function myFunctionThree() {
-    document.getElementById("dropdown-menu-three").classList.toggle("show");
-}
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn-three')) {
-
-        var dropdown2 = document.getElementsByClassName("dropdown-content-three");
-        var i;
-        for (i = 0; i < dropdown2.length; i++) {
-            var openDropdown2 = dropdown2[i];
-            if (openDropdown2.classList.contains('show')) {
-                openDropdown2.classList.remove('show');
-            }
-        }
-    }
-};
-
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -54,13 +37,14 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
+function removeNode(node) {
+    node.parentNode.removeChild(node);
+}
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     var drag_depo = ['drag1','drag2'];
-    var drag_cont = ['text_box','text_area','html_editor','check_box',
-        'datapicker','dropdown','radiobutton','lookup','grid','label','field_text','field_area',
-        'field_date', 'field_number', 'field_drop_down', 'field_checkbox', 'field_radio_button', 'field_decimal'];
+    var drag_cont = ['text_box'];
     var isLeft;
     if(drag_depo.includes(data)){
         isLeft = 'depositories';
@@ -77,36 +61,36 @@ function drop(ev) {
         ev.target.appendChild(element);
 
          if(data ==='drag1' && element.parentNode.id ==='frame'){
-             element.classList.add("grid-stack-item","grid-stack-item");
-             element.innerHTML ="<div class='grid-stack'></div>";
+             element.classList.add("container-fluid","row");
+             element.innerHTML =`<div class="grid-stack-item" data-gs-x="4" data-gs-y="0" data-gs-width="12" data-gs-height="8" id="drop">
+                 <div class="grid-stack-item-content">
+                         <div class="grid-stack">
+                             <div class="grid-stack-item" data-gs-x="0" data-gs-y="0" data-gs-width="3" data-gs-height="1"><div class="grid-stack-item-content">1</div></div>
+                             <div class="grid-stack-item" data-gs-x="3" data-gs-y="0" data-gs-width="3" data-gs-height="1"><div class="grid-stack-item-content">2</div></div>
+                             <div class="grid-stack-item" data-gs-x="6" data-gs-y="0" data-gs-width="3" data-gs-height="1"><div class="grid-stack-item-content">3</div></div>
+                             <div class="grid-stack-item" data-gs-x="9" data-gs-y="0" data-gs-width="3" data-gs-height="1"><div class="grid-stack-item-content">4</div></div>
+                             <div class="grid-stack-item" data-gs-x="0" data-gs-y="1" data-gs-width="3" data-gs-height="1"><div class="grid-stack-item-content">5</div></div>
+                             <div class="grid-stack-item" data-gs-x="3" data-gs-y="1" data-gs-width="3" data-gs-height="1"><div class="grid-stack-item-content">6</div></div>
+                             <div class="grid-stack-item" data-gs-x="0" data-gs-y="1" data-gs-width="3" data-gs-height="1"><div class="grid-stack-item-content">7</div></div>
+                             <div class="grid-stack-item" data-gs-x="3" data-gs-y="1" data-gs-width="4" data-gs-height="1"><div class="grid-stack-item-content">8</div></div>
+                         </div>
+                 </div>
+             </div>`;
              return element;
          }
-         else if(data === 'drag2' && element.parentNode.classList.contains("grid-stack")) {
-             element.classList.add("grid-stack-item", "grid-stack-item-content");
-             elemant.setAttribute('data-gs-width', '3');
-             element.innerHTML ="<div><span class='card-title'>Card Title</span>" +
-                 "<div class='right'>" +
-                 "<span class='glyphicon glyphicon-pencil'></span>" +
-                 "<span class='glyphicon glyphicon-trash'></span></div>" +
-                 "</div>" +
-                 "<div class='card'>"+
-                 "</div>";
-             // element.draggable();
+         if(data === 'drag2' && element.parentNode.classList.value === 'block') {
+             element.classList.add("grid-stack");
+             element.innerHTML =`<div class='col-md-4 col-xs-12'>
+             <div class="grid-stack-item" data-gs-width="4" data-gs-height="1"><div class="grid-stack-item-content">
+             1</div></div></div>`;
              return element;
          }
-        else if(data === 'drag2' && element.parentNode.id ==='frame') {
+        if(data === 'drag2' && element.parentNode.id ==='frame') {
             element.classList.add("block");
-            element.innerHTML ="<div class='cards col-md-4'><div><span class='card-title'>Card Title</span>" +
-                "<div class='right'>" +
-                "<span class='glyphicon glyphicon-pencil'></span>" +
-                "<span class='glyphicon glyphicon-trash'></span></div>" +
-                "</div>" +
-                "<div class='card'>"+
-                "</div></div>";
+            element.innerHTML =`<div class='grid-stack'><div class='col-md-4 col-xs-12'>
+            <div class="grid-stack-item" data-gs-width="4" data-gs-height="1"><div class="grid-stack-item-content">
+            1</div></div></div></div>`;
             return element;
-        }
-        else {
-            element.remove();
         }
 
     }
@@ -115,226 +99,25 @@ function drop(ev) {
         var element = document.createElement("div");
         ev.target.appendChild(element);
 
-        //Create templates
-
-
-        var text_box = "<div class='items-fields'>" +
-            "<label for='text_box'>Text</label>" +
-            "<input  type='text'>" +
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-
-        var text_area ="<div class='items-fields'>" +
-            "<label for='textarea'>Textarea</label>" +
-            "<textarea class='textarea'></textarea>" +
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-        var html_editor="";
-
-        var check_box = "<div class='items-fields'>" +
-            "<input type='checkbox'/>" +
-            "<label for='test'>Red</label>" +
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-        var datapicker = "<div class='items-fields'>" +
-            "<input type='date' class='datepicker'>" +
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-        var dropdown = "<div class='dropdown items-fields'>"+
-            "<button class='btn btn-info dropdown-toggle' type='button' data-toggle='dropdown'>Dropdown Example"+
-            "<span class='caret'></span></button>"+
-            "<ul class='dropdown-menu'>" +
-            "<li><a href='#'>HTML</a></li>" +
-            "<li><a href='#'>CSS</a></li>" +
-            "<li><a href='#'>JavaScript</a></li>" +
-            "</ul>" +
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-
-        var radiobutton="<div class='items-fields'>" +
-            "<input name='group' type='radio'/>"+
-            "<label for='test'>Red</label>" +
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-        var lookup="";
-        var grid="";
-
-        var label="<div class='items-fields'>" +
-            "<label>Label</label>" +
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-
-        var field_number = "<div class='items-fields'>"+
-            "<label for='num'>Number</label>" +
-            "<input type='number' name='num' min='1' max='5'>"+
-            "<div class='right'>" +
-            "<span class='glyphicon glyphicon-pencil'></span>" +
-            "<span class='glyphicon glyphicon-remove'></span></div>" +
-            "</div>";
-        var field_decimal = "<div class='items-fields'>"+
-        "<label for='decimal'>Decimal</label>" +
-        "<input type='number' required name='decimal' min='0' value='0' step='.01'>"+
-        "<div class='right'>" +
-        "<span class='glyphicon glyphicon-pencil'></span>" +
-        "<span class='glyphicon glyphicon-remove'></span></div>" +
-        "</div>";
-
-        var group_field;
-
-
-        var template;
-
-        //Get dragged element and set template for this element
-        switch (data) {
-            // Controls
-            case 'text_box':
-                template = text_box;
-                break;
-            case 'text_area':
-                template = text_area;
-                break;
-            case 'html_editor':
-                template = html_editor;
-                break;
-            case 'check_box':
-                template = check_box;
-                break;
-            case 'datapicker':
-                template = datapicker;
-                break;
-            case 'dropdown':
-                template = dropdown;
-                break;
-            case 'radiobutton':
-                template = radiobutton;
-                break;
-            case 'lookup':
-                template = lookup;
-                break;
-            case 'grid':
-                template = grid;
-                break;
-            case 'label':
-                template = label;
-                break;
-            // Fields
-            case 'field_text':
-                template = text_box;
-                break;
-            case 'field_area':
-                template = text_area;
-                break;
-            case 'field_date':
-                template = datapicker;
-                break;
-            case 'field_checkbox':
-                template = check_box;
-                break;
-            case 'field_drop_down':
-                template = dropdown;
-                break;
-            case 'field_radio_button':
-                template = radiobutton;
-                break;
-            case 'field_number':
-                template = field_number;
-                break;
-            case 'field_decimal':
-                template = field_decimal;
-                break;
-        }
-
-        if( element.parentNode.classList.contains("card")){
+        if(data === 'text_box' && element.parentNode.classList.value === 'cards col-md-4'){
             element.classList.add("field");
-            element.innerHTML = template;
+            element.innerHTML ="<input type='text'>";
             return element;
 
         }
-        if(element.parentNode.classList.contains("block")){
+        if(data === 'text_box' && element.parentNode.classList.value === 'block'){
             element.classList.add("cards","col-md-4");
-            element.innerHTML ="<div><span class='card-title'>Card Title</span>" +
-                "<div class='right'>" +
-                "<span class='glyphicon glyphicon-pencil'></span>" +
-                "<span class='glyphicon glyphicon-trash'></span></div>" +
-                "</div>" +
-                "<div class='card'>"+
-                "<div class='field'>" + template + "</div></div>";
+            element.innerHTML ="<div class='field'><input type='text'></div>";
             return element;
 
         }
-        if(element.parentNode.id ==='frame'){
+        if(data === 'text_box' && element.parentNode.id ==='frame'){
             element.classList.add("container-fluid","row");
-            element.innerHTML ="<div class='block'><div class='cards col-md-4'>" +
-                "<div><span class='card-title'>Card Title</span>" +
-                "<div class='right'>" +
-                "<span class='glyphicon glyphicon-pencil'></span>" +
-                "<span class='glyphicon glyphicon-trash'></span></div>" +
-                "</div>" +
-                "<div class='card'>"+
-                "<div class='field'>" + template + "</div></div>"+
-                "</div></div>";
+            element.innerHTML ="<div class='block'><div class='cards col-md-4'><div class='field'><input type='text'></div></div></div>";
             return element;
-        }
-        if(!element.parentNode.classList.contains("cards")  && !element.parentNode.classList.contains("block") && element.parentNode.id != 'frame') {
-            element.remove();
         }
 
     }
-
     ev.stopPropagation();
     return false;
 }
-
-$("#frame").sortable({
-    axis: "y",
-    items: ".row"
-});
-
-$('#frame').on('mousedown','.block',function(){
-
-
-        $(".block").sortable({
-            items: ".cards",
-        });
-
-        $(".card").sortable({
-            items: ".field"
-        });
-
-        // $( ".field" ).draggable({
-        //     //items:".field",
-        //     containment: "parent"
-        // });
-        var classname = document.getElementsByClassName("glyphicon-trash");
-        var delete_field = document.getElementsByClassName("glyphicon-remove");
-
-        var myFunction = function() {
-            $(this).closest('.cards').remove();
-        };
-        var deleteField = function () {
-            $(this).closest('.field').remove();
-        };
-
-        Array.from(classname).forEach(function(element) {
-            element.addEventListener('click', myFunction);
-        });
-        Array.from(delete_field).forEach(function(element) {
-            element.addEventListener('click', deleteField);
-        });
-
-
-});
