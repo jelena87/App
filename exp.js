90b3022cf769522c9f034ee363e5f458db5d8e9d
jelena87 <jelena_zvnvc@yahoo.com>
2017-08-03 19:52:17 +0200

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,28 @@
+var container = $("#container");
+var box = $(".box");
+
+var width  = 80;
+var height = 80;
+var rows   = 8;
+var cols   = 12;
+var snap   = 80;
+
+for (var i = 0; i < rows * cols; i++) {
+    var y = Math.floor(i / cols) * height;
+    var x = (i * width) % (cols * width);
+    $("<div grid-cell></div>").css({ top: y, left: x }).prependTo(container);
+}
+
+Draggable.create(box, {
+    bounds: container,
+    onDrag: onDrag
+});
+
+function onDrag() {
+
+    TweenLite.to(box, 0.5, {
+        x: Math.round(this.x / snap) * snap,
+        y: Math.round(this.y / snap) * snap,
+        ease: Back.easeOut.config(2)
+    });
+}
